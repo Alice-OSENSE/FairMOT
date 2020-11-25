cf7a15854d217525fd199615ff9a57fc5dbe0758
Alice-OSENSE <alice.li@osensetech.com>
2020-11-25 10:20:36 +0800

    Add script to run demo

---
+++ src/scripts/demo_mot.sh
@@ -0,0 +1,7 @@
+python3 ../demo.py mot \
+--load_model "/home/osense-office/Documents/ext_repo/FairMOT/models/crowdhuman_dla34.pth" \
+--conf_thres 0.4 \
+--input-video "/home/osense-office/Documents/sample_video/basketball.mp4" \
+--output-root "/home/osense-office/Documents/ext_repo/FairMOT/results" \
+--output-format text \
+--num-class 1
